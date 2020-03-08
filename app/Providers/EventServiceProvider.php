2557587750c9e6a10d80c7a3b777c38370825fbd
Nooralam khan <CoreyMShafer@gmail.com>
2020-03-08 10:47:50 +0600

    usercreated event and its llistener in listen array

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,7 +6,8 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-
+use App\Events\UserCreated;
+use App\Listeners\UserCreatedListener;
 class EventServiceProvider extends ServiceProvider
 {
     /**
@@ -15,9 +16,12 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
-        ],
+        // Registered::class => [
+        //     SendEmailVerificationNotification::class,
+        // ],
+        UserCreated::class =>[
+            UserCreatedListener::class
+        ]
     ];
 
     /**
