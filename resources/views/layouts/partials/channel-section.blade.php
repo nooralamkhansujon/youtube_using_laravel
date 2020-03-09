bce6a61ad348cbde624c4f1bbedf2f063740b167
Nooralam khan <CoreyMShafer@gmail.com>
2020-03-09 20:40:42 +0600

    channel view added

---
+++ resources/views/layouts/partials/channel-section.blade.php
@@ -0,0 +1,83 @@
+<div class="d-flex justify-content-between mb-4 border-bottom pb-3">
+    <h1 class="text-secondary">Featured Channels</h1>
+</div>
+
+{{-- //featured channels section  --}}
+<div class="row mb-4">
+    <div class="col-md-3">
+            <div class="card">
+                <div class="card-body ml-3">
+                   <img src="{{asset('image/user.jpg')}}" width="100px" height="100px" class="rounded-circle ml-3" alt="">
+                   <h4 class="text-danger">
+                       Anupam Movies
+                   </h4>
+                   <h5 class="text-secondary">
+                       <span>1.65M</span>
+                       Subscribers
+                    </h5>
+                   <button class="btn btn-danger">Subscribe</button>
+                </div>
+            </div>
+       </div>
+       <div class="col-md-3">
+        <div class="card">
+            <div class="card-body ml-3">
+               <img src="{{asset('image/user.jpg')}}" width="100px" height="100px" class="rounded-circle ml-3" alt="">
+               <h4 class="text-danger">
+                   Anupam Movies
+               </h4>
+               <h5 class="text-secondary">
+                   <span>1.65M</span>
+                   Subscribers
+                </h5>
+               <button class="btn btn-danger">
+                   Subscribe
+                </button>
+            </div>
+        </div>
+   </div>
+
+</div>
+{{-- end of featured channel  --}}
+
+<div class="d-flex justify-content-between mb-4 border-bottom pb-3">
+    <h1 class="text-secondary">Subscription Channels</h1>
+</div>
+
+{{-- //featured channels section  --}}
+<div class="row mb-4">
+    <div class="col-md-3">
+            <div class="card">
+                <div class="card-body ml-3">
+                   <img src="{{asset('image/user.jpg')}}" width="100px" height="100px" class="rounded-circle ml-3" alt="">
+                   <h4 class="text-danger">
+                       Anupam Movies
+                   </h4>
+                   <h5 class="text-secondary">
+                       <span>1.65M</span>
+                       Subscribers
+                    </h5>
+                   <button class="btn btn-danger">Subscribe</button>
+                </div>
+            </div>
+       </div>
+       <div class="col-md-3">
+        <div class="card">
+            <div class="card-body ml-3">
+               <img src="{{asset('image/user.jpg')}}" width="100px" height="100px" class="rounded-circle ml-3" alt="">
+               <h4 class="text-danger">
+                   Anupam Movies
+               </h4>
+               <h5 class="text-secondary">
+                   <span>1.65M</span>
+                   Subscribers
+                </h5>
+               <button class="btn btn-danger">
+                   Subscribe
+                </button>
+            </div>
+        </div>
+   </div>
+
+</div>
+{{-- end of featured channel  --}}
