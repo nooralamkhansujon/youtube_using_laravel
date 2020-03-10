ec7f7e992da02b5529b4fa9682538a756659383c
Nooralam khan <CoreyMShafer@gmail.com>
2020-03-10 21:05:17 +0600

    profile-section view modified

--- resources/views/layouts/partials/profile-section.blade.php
+++ resources/views/layouts/partials/profile-section.blade.php
@@ -1,27 +1,49 @@
 {{-- start of profile  --}}
 <div class="card">
     <div class="card-header">
-        <img src="{{asset('image/user.jpg')}}" class="rounded-circle" width="150px" height="150px" alt="">
+        <img src="{{(isset($profile->image->url))?asset('storage/'.$profile->image->url):asset('image/user.jpg')}}"
+        class="rounded-circle" width="150px" height="150px" alt="">
     </div>
     <div class="card-body">
-        <form action="" method="POST">
+        @if(session()->has('message'))
+           <div class="alert alert-{{session()->get('type')}}">
+                {{ session()->get('message') }}
+            </div>
+        @endif
+
+       <form action="{{route('profile.update')}}" method="POST" enctype="multipart/form-data">
+            @csrf
             <div class="col-md-12 form-group">
-                <label class="form-control-label text-danger" style="font-size:24px" for="fullname">Full Name</label>
-              <input type="text" name="fullname" value="{{$profile->fullname}}" id="fullname" class="form-control">
+                <label class="form-control-label text-secondary" style="font-size:24px" for="fullname">Full Name</label>
+                <input type="text" name="fullname" value="{{$profile->fullname}}" id="fullname" class="form-control">
+                @if($errors->has('fullname'))
+                   <span class="text-danger">
+                       {{$errors->first('fullname')}}
+                    </span>
+                @endif
             </div>
             <div class="col-md-12 form-group">
-                <label class="form-control-label text-danger" style="font-size:24px" for="location">Location</label>
+                <label class="form-control-label text-secondary" style="font-size:24px" for="location">Location</label>
                 <input type="text" name="location" id="location" value="{{$profile->location}}" class="form-control">
+                @if($errors->has('location'))
+                   <span class="text-danger">{{$errors->first('location')}}</span>
+                @endif
             </div>
             <div class="col-md-12 form-group">
-                <label class="form-control-label text-danger" style="font-size:24px" for="email">Email</label>
+                <label class="form-control-label text-secondary" style="font-size:24px" for="email">Email</label>
                 <input type="email" name="email" id="email" value="{{$profile->user->email}}" class="form-control">
+                @if($errors->has('email'))
+                  <span class="text-danger">{{$errors->first('email')}}</span>
+                @endif
             </div>
             <div class="col-md-12 form-group">
-                <img src="{{asset('image/user.jpg')}}" width="50px" height="50px" alt="">
-                <label class="form-control-label text-danger"
+                <img src="{{(isset($profile->image))?asset('storage/'.$profile->image->url):asset('image/user.jpg')}}" width="50px" height="50px" alt="">
+                <label class="form-control-label text-secondary"
                 style="font-size:24px;display:block;" for="image">Image</label>
-                <input type="file" name="image"id="image" accept="image/*" >
+                <input type="file" name="image" id="image"  >
+                @if($errors->has('image'))
+                   <span class="text-danger">{{$errors->first('image')}}</span>
+                @endif
             </div>
             <div class="col-md-12 form-group">
                 <input type="submit" class="btn btn-danger btn-lg" value="Update" name="update">
@@ -32,31 +54,43 @@
 {{-- end of profile  --}}
 
 {{-- start of channel  --}}
-    <div class="card mb-3">
+    <div class="card mb-3 mt-3">
         <div class="d-flex justify-content-between">
-               <h1 class="text-secondary card-header border-bottom pb-2">Create Channel</h1>
+               <h1 class="text-secondary p-2">Create Channel</h1>
                <button class="btn btn-danger" id="create_channel">Create channel</button>
         </div>
 
         <div class="card-body channel" id="channel_body">
-            <form action="" method="POST">
+            @if(session()->has('msg'))
+                <div class="alert alert-{{session()->get('type')}}">
+                    {{ session()->get('msg') }}
+               </div>
+            @endif
+        <form action="{{route('channel.create')}}" method="POST" enctype="multipart/form-data">
+                @csrf
                 <div class="col-md-12 form-group">
                     <label for="name" class="form-control-label text-danger" style="font-size:20px;text-transform:capitalize">
-                        Channel Name</label>
+                        Channel Name
+                    </label>
                     <input type="text" name="name" placeholder="Enter Channel Name" class="form-control" id="name">
                 </div>
                 <div class="col-md-12 form-group">
-                     <label for="description" class="form-control-label text-danger" style="font-size:20px;text-transform:capitalize;">Description</label>
-                     <textarea type="text" name="description" placeholder="Enter Channel Description" class="form-control" id="description">
+                    <label for="description" class="form-control-label text-danger"
+                        style="font-size:20px;text-transform:capitalize;">
+                        Description
+                    </label>
+                     <textarea type="text" name="description" placeholder="Enter Channel Description"
+                     class="form-control" id="description">
                      </textarea>
                 </div>
                 <div class="col-md-12 form-group">
-                     <label for="image" class="form-control-label text-danger" style="font-size:20px;text-transform:capitalize;">Image</label>
-                     <input type="file" name="image" class="form-control" accept="image/*">
-                 </div>
-                 <div class="col-md-12 form-group">
+                     <label for="image" class="form-control-label text-danger"
+                     style="font-size:20px;text-transform:capitalize;">Image</label>
+                     <input type="file" name="image" class="form-control">
+                </div>
+                <div class="col-md-12 form-group">
                      <input type="submit" name="channel" value="Create Channel" class="btn btn-danger">
-                 </div>
+                </div>
            </form>
         </div>
     </div>
