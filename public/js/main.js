6370ec2be6ec67a031043ff274f3765594a825b3
Nooralam khan <CoreyMShafer@gmail.com>
2020-03-11 10:57:31 +0600

    main.js file has been modified

--- public/js/main.js
+++ public/js/main.js
@@ -3,37 +3,44 @@ const tabSection = document.querySelectorAll('.tab-section');
 
 
 
-// console.log(tabItem);
-function init()
+// // console.log(tabItem);
+// function init()
+// {
+//     tabItem.forEach((element)=>{
+//         element.addEventListener('click',function(){
+//                 removeBorder();
+//                 removeShowClass();
+//                 //get index
+//                 index = this.dataset.index;
+//                 //select tab-section by index
+//                 section = document.getElementById(`tab-section-${index}`);
+//                 //add show class in section
+//                 section.classList.add('show');
+//                 //add tab-border in manu item
+//                 this.classList.add('tab-border');
+//         });
+//     });
+// }
+
+
+// function removeBorder()
+// {
+//     tabItem.forEach(element=>element.classList.remove('tab-border'));
+// }
+
+// function removeShowClass()
+// {
+//     tabSection.forEach(element=>element.classList.remove('show'))
+// }
+// init();
+
+function createChannel()
 {
-    tabItem.forEach((element)=>{
-        element.addEventListener('click',function(){
-                removeBorder();
-                removeShowClass();
-                //get index
-                index = this.dataset.index;
-                //select tab-section by index
-                section = document.getElementById(`tab-section-${index}`);
-                //add show class in section
-                section.classList.add('show');
-                //add tab-border in manu item
-                this.classList.add('tab-border');
-        });
+    let create_channel_button = document.getElementById('create_channel');
+    let create_channel = document.getElementById('channel_body');
+    create_channel_button.addEventListener('click',function(){
+            create_channel.classList.toggle('show');
     });
 }
+createChannel()
 
-
-function removeBorder()
-{
-    tabItem.forEach((element)=>{
-        element.classList.remove('tab-border');
-    })
-}
-
-function removeShowClass()
-{
-    tabSection.forEach(element=>{
-        element.classList.remove('show');
-    })
-}
-init();
