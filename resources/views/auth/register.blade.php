b32c039f45b6b7ffb65a164046d07b9374956aac
Nooralam khan <CoreyMShafer@gmail.com>
2020-03-12 13:22:01 +0600

    register view modified

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 offset-4">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header text-danger">{{ __('Register') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
