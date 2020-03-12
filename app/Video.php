520013a082f1db974713385ebb3d182ad9261dce
Nooralam khan <CoreyMShafer@gmail.com>
2020-03-12 13:37:59 +0600

    polymorphic relationalship has been defined with likes and dislikes table

--- app/Video.php
+++ app/Video.php
@@ -3,8 +3,33 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Channel;
+use App\View;
+use App\Like;
+use App\Dislike;
 
 class Video extends Model
 {
-    //
+    protected $guarded=[];
+
+    public function channel()
+    {
+        return $this->belongsTo(Channel::class,'channel_id','id');
+    }
+
+    public function views()
+    {
+        return $this->hasMany(View::class,'video_id','id');
+    }
+
+    public function likes()
+    {
+        return $this->morphToMany(Like::class,'likeable');
+    }
+
+
+    public function dislikes()
+    {
+        return $this->morphToMany(Dislike::class,'dislikeable');
+    }
 }
