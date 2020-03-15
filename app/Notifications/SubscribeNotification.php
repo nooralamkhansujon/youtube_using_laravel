ec64ed5ebd45b9959c5139688370ec9022514c34
Nooralam khan <CoreyMShafer@gmail.com>
2020-03-15 20:13:59 +0600

    create_notifications_table is create for notifications

---
+++ app/Notifications/SubscribeNotification.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class SubscribeNotification extends Notification
+{
+    use Queueable;
+    public $user;
+    public $channel_user;
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user  = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['database'];
+    }
+
+    // /**
+    //  * Get the mail representation of the notification.
+    //  *
+    //  * @param  mixed  $notifiable
+    //  * @return \Illuminate\Notifications\Messages\MailMessage
+    //  */
+    // public function toMail($notifiable)
+    // {
+    //     return (new MailMessage)
+    //                 ->line('The introduction to .')
+    //                 ->action('Notification Action', url('/'))
+    //                 ->line('Thank you for using our application!');
+    // }
+
+    public function toDatabase($notifiable)
+    {
+        if($this->user == $notifiable->name)
+        {
+            $message = "<strong>You</strong> subscribe on your Channel";
+        }
+        else{
+
+            $message = "<strong>".$this->user->name."</strong> subscribe on your Channel";
+        }
+        return [
+            'message' => $message
+        ];
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    // public function toArray($notifiable)
+    // {
+    //     return [
+    //         //
+    //     ];
+    // }
+}
