c40451ccf06a1070b351c6aa136d7db609bd8b10
Nooralam khan <CoreyMShafer@gmail.com>
2020-03-17 12:09:37 +0600

    store function completed in commentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Comment;
 use Illuminate\Http\Request;
+use App\Notifications\CommentNotification;
+use App\Comment;
+use App\Video;
 
 class CommentController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -35,7 +41,23 @@ class CommentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        //save comment in database
+         $comment                   = new Comment();
+         $comment->from_id          = auth()->user()->id;
+         $comment->comment          = $request->comment;
+         $comment->commentable_id   = $request->video_id;
+         $comment->commentable_type = 'App\Video';
+         $comment->save();
+
+         //  show notification to the video user
+         $video_user                = Video::find($request->video_id)->channel->user;
+         $video_user->notify(new CommentNotification(auth()->user()));
+
+        $output = array(
+            'message' => "ok",
+        );
+        return $output;
     }
 
     /**
@@ -60,26 +82,6 @@ class CommentController extends Controller
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Comment $comment)
-    {
-        //
-    }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Comment  $comment
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Comment $comment)
-    {
-        //
-    }
+
 }
